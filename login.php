ff34867d45e93338929977937b85cf49a8ec461c
mohamed <37941160+ems3c@users.noreply.github.com>
2022-06-27 08:26:15 +0000

    Update login.php
    Added some sanitizations

--- login.php
+++ login.php
@@ -1,6 +1,13 @@
 <?php
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    
+    /*
+    Remeber to do some filtering and some sanitization in those POST params otherwise we gonna cause SQLi and other attacks
+    */
+    $username = filter_var($_POST["username"], FILTER_SANITIZE_STRING);
+    $password = filter_var($_POST["password"], FILTER_SANITIZE_STRING);
+
+    //We are good to go
+
     $postdata = "username="+$username+"&"+"password="+$password
     if (isset($username) && isset($password)) {
         if ($username !== "" && $password !== "") {
